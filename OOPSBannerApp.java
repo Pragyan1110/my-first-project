865bfed04dfadf862819a69c0cba4cdcf6a96cda
Pragyan1110 <pragyan.mehta@gmail.com>
2026-02-22 18:30:19 +0530

    UC3: Refactor banner using String.join() for memory efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,23 @@
-/**
- * OOPSBannerApp
- * Displays the word "OOPS" in large ASCII banner format.
- *
- * @author Pragyan
- * @version 2.0
- */
-public class OOPSBannerApp {
+public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    System.out.println(String.join("  ",
+            " ***** ", " ***** ", " ******", " ***** "));
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * *     *  ***** ");
-        System.out.println("*     * *     * * ****       *");
-        System.out.println("*     * *     * *       *     *");
-        System.out.println(" *****   *****  *        ***** ");
+    System.out.println(String.join("  ",
+            "*     *", "*     *", "*     *", "*     *"));
 
-    }
+    System.out.println(String.join("  ",
+            "*     *", "*     *", "*     *", "*      "));
 
+    System.out.println(String.join("  ",
+            "*     *", "*     *", "****** ", " ***** "));
+
+    System.out.println(String.join("  ",
+            "*     *", "*     *", "*      ", "      *"));
+
+    System.out.println(String.join("  ",
+            "*     *", "*     *", "*      ", "*     *"));
+
+    System.out.println(String.join("  ",
+            " ***** ", " ***** ", "*      ", " ***** "));
 }
